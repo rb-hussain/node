2d83da90f036d557d9f1bc550506a1785244d62e
Rabi <rbhussain990@gmail.com>
2023-03-24 12:07:21 +0500

    Server is created using HTTP

--- index.js
+++ index.js
@@ -2,19 +2,33 @@
 // const a = 400 ;
 
 // module.exports = a ;
-const fs = require ("fs");
+// const fs = require ("fs");
 
-fs.readFile("./nodejs.txt", "utf8", (err,data) =>{
-if(err){
-    throw err;
-}
-    console.log(data);
+// fs.readFile("./nodejs.txt", "utf8", (err,data) =>{
+// if(err){
+//     throw err;
+// }
+//     console.log(data);
 
+// });
+
+// const b = "This is demo text";
+
+// fs.writeFile("./demo.txt", b ,()=>{
+//     console.log("Written")
+// });
+// console.log("I'm first");
+// console.log("Checking Nodemon");
+
+// Creating a Server 
+const http = require("http");
+
+const PORT = 2000;
+const hostname = "localhost";
+const server =  http.createServer((req,res)=>{
+    res.end("working")
 });
 
-const b = "This is demo text";
-
-fs.writeFile("./demo.txt", b ,()=>{
-    console.log("Written")
+server.listen(PORT,hostname, ()=>{
+    console.log(`Server is working on ${hostname}:${PORT}`)
 });
-console.log("I'm first");
